9f1e5719e47b6f4b5de593ff27576b05a941be81
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-08 18:17:28 -0300

    Update

--- JavaClasses/src/javaclasses/JavaClasses.java
+++ JavaClasses/src/javaclasses/JavaClasses.java
@@ -3,8 +3,36 @@ package javaclasses;
 public class JavaClasses {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Reader reader = new Reader();
+		
+		reader.readIt(new IReadable(){
+			
+			@Override
+			public String read() {
+				throw new UnsupportedOperationException();
+			}
+		});
 
 	}
 
+}
+
+interface IReadable{
+	String read();
+	
+}
+
+class Reader{
+	String text;
+	
+
+
+public void readIt(IReadable readMaterial) {
+	text = readMaterial.read();
+}
+
+public void printIt() {
+	if (text != null)
+		System.out.println(text);
+	}
 }
